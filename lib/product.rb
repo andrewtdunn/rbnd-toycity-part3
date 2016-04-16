7dbf9178fb73c299af7814bc192d0fd8f8154340
Andrew Dunn <atd2005@gmail.com>
2016-04-16 00:39:35 -0400

    raise DuplicateProduct error if title is in the array already

--- lib/product.rb
+++ lib/product.rb
@@ -1,11 +1,21 @@
 class Product
+    attr_reader :title
     @@products  = []
 
     def initialize(options={})
-        @@products << self
+        @title = options[:title]
+        add_to_products
     end
 
     def self.all
         @@products
+    end
+
+    def add_to_products
+        if @@products.any?{|product| product.title == @title}
+            raise DuplicateProductError, @title
+        else
+            @@products << self
+        end
     end
 end
